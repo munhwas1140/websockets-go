b5bc99a25d1ba27ee1c1e63692218db39867b627
Hyung Jun Noh <munhwas1140@gmail.com>
2024-07-02 22:38:58 +0900

    first commit

---
+++ client.go
@@ -0,0 +1,15 @@
+package main
+
+import "github.com/gorilla/websocket"
+
+type Client struct {
+	connection *websocket.Conn
+	manager    *Manager
+}
+
+func NewClient(conn *websocket.Conn, manager *Manager) *Client {
+	return &Client{
+		connection: conn,
+		manager:    manager,
+	}
+}
